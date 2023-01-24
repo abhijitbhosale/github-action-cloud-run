46681c5b24f95188f828226dbae37e50bf55c28f
abhijitbhosale <abhijitbhosale96@gmail.com>
2023-01-24 19:29:30 +0530

    Added Files
    Added Files

---
+++ src/main/java/com/githubaction/cloudrun/githubactioncloudrun/GithubActionCloudRunApplication.java
@@ -0,0 +1,13 @@
+package com.githubaction.cloudrun.githubactioncloudrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionCloudRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionCloudRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubaction/cloudrun/githubactioncloudrun/GithubActionCloudRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubaction.cloudrun.githubactioncloudrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionCloudRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
